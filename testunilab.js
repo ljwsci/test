bdfa7e5bc510403b3e2ab3f7270aba71e3d0cd89
liujiawen2018phyche <97917263+liujiawen2018phyche@users.noreply.github.com>
2022-03-16 16:08:24 +0800

    labtest
    test only

---
+++ testunilab.js
@@ -0,0 +1,11 @@
+>>>this is a test project
+>>>when utc+8=6:00:00;
+>>>labserver=Y;
+>>>labdatabases=Y;
+>>>Setting IP Adress::;
+>>>start//::
+>>>IP icde=192.168.0.1
+>>>ita=all ipt ~.255;//
+>>>finish;//
+>>>end all;
+>>>end;
