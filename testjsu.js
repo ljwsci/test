97dad824d147085fa48052d622ac38eac912b0d8
liujiawen2018phyche <97917263+liujiawen2018phyche@users.noreply.github.com>
2022-04-12 19:43:34 +0800

    Add files via upload

---
+++ testjsu.js
@@ -0,0 +1,16 @@
+UNI_License
+/start all/:/;
+/:
+use uni.logic_ver.7;
+use uni.steruaer_ver.1;
+uni.puarsuer
+su UNI_CNSER_admin;
+su UNI_CNDB_admin;
+cd etc/UNI./suer/test/AI/AISS/admin/LICENSECENTER/UNI.2.A/UNI.SCI.P.C./admin/;
+ctrl all ecpt UNI;
+su admin;
+cd etc/UNI;
+test -aux;
+end all;
+:/
+/finish all/;
