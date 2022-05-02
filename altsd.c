5917d8bb6e98f43031a1f74d6292e97aebb0922f
liujiawen2018phyche <97917263+liujiawen2018phyche@users.noreply.github.com>
2022-05-02 19:50:49 +0800

    Add files via upload

---
+++ altsd.c
@@ -0,0 +1,11 @@
+>>>start all process//
+>>>test process; 
+>>>start all program//
+>>>test program;
+>>>finish all// 
+>>>test the server a;
+>>>test the server c;
+>>>test  server ip;
+>>>end all;
+>>>end program;
+>>>finish;
