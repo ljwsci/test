dc46df7bf0c2274b653a2e35115a2fa9d36cac89
liujiawen2018phyche <97917263+liujiawen2018phyche@users.noreply.github.com>
2022-08-03 13:09:15 +0800

    Add files via upload

---
+++ maintest.js
@@ -0,0 +1,10 @@
+>>>test url:/
+>>>ubr test iul=127.0.0.1:8080;
+>>>ubr test iul=255.255.255.0:443;
+>>>int ip;
+>>>set ip = <.>?123456789;
+>>>set def debugger uniqueID = 0977;
+>>>run all -aux -f -s -t 600 -n 10;
+>>>run bacil = ini u[data . ];
+>>>end all;
+>>>end;
