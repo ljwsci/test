b6d968d96473faa0d022b1eaebc17b309e6365dc
liujiawen2018phyche <97917263+liujiawen2018phyche@users.noreply.github.com>
2022-12-09 00:35:45 +0800

    Add files via upload

---
+++ testutc.c
@@ -0,0 +1,10 @@
+>>>//test utc time and tc
+>>>set GMT = UTC;
+>>>set server_time = UTC + 8;
+>>>set tcc = 0 + 15t;
+>>>if tcp = E ,
+>>>then set as t>0;
+>>>others <0;
+>>>end if;
+>>>end;
+>>>finish;
