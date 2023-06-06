343351fe4e548643ecbe6d917554fab920003447
liujiawen2018phyche <97917263+liujiawen2018phyche@users.noreply.github.com>
2023-06-06 19:39:19 +0800

    Add files via upload

---
+++ acdbsu.c
@@ -0,0 +1,17 @@
+>>>start requry
+Like IIf (IsNull([Forms]![Windowsname]![words]),'*','*'&[Forms]![Windowsname]![words]&'*');
+set name = words;
+end requry;
+>>>start VBA_R {
+    Private Sub <idname>
+    Me.s_windowsname.Requery
+    End Sub
+};
+end process VBA_R;
+>>>start VBA_C {
+    Private Sub <idcrname>
+    Me.wordsname.Value = IsNull
+    Me.s_windowsname.Requery
+    End Sub
+};
+end process VBA_C;
