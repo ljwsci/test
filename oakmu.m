bbf57eed1e0c30cebc887cf101753e1be4265834
liujiawen2018phyche <97917263+liujiawen2018phyche@users.noreply.github.com>
2023-07-06 19:09:31 +0800

    Add files via upload

---
+++ oakmu.m
@@ -0,0 +1,12 @@
+>>>set db as testdb_"模拟3";
+>>>start uslop = 0000.00000.0000.0000+0000.0000.0000.0000;
+>>>end set base;
+>>>start aaa
+{
+	if ip = :::0
+	then set iodine= 1；
+	other0；
+}；
+end base;
+end all;
+end;
