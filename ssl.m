981d5945756bb8552728bfdaae41ee18219dc4ba
liujiawen2018phyche <97917263+liujiawen2018phyche@users.noreply.github.com>
2024-02-11 19:34:44 +0800

    Add files via upload

---
+++ ssl.m
@@ -0,0 +1,9 @@
+start ssl_setup
+{
+openssl genrsa -out key.pem 2048
+openssl req -new -key key.pem -out csr.pem
+openssl x509 -req -in csr.pem -out cert.pem -signkey key.pem -days 365
+};
+end process;
+end all;
+end;
